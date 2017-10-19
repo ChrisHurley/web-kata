452aa039ca75f1a251f888572ce55eef1d4f62d8
Chris Hurley <chris.hurley@red-gate.com>
2017-10-19 17:14:17 +0100

    Add expand toggle

--- app3/src/Products.js
+++ app3/src/Products.js
@@ -2,11 +2,28 @@ import React, { Component } from 'react'
 import './Products.css'
 
 class Product extends Component{
+    constructor(props){
+        super(props)
+        this.state= { expanded: false }
+      }
+
+    toggleExpanded = (evt) => {
+        evt.preventDefault();
+        this.setState((currentState) => { 
+            return { expanded: !currentState.expanded }
+         });
+    }
+
     render(){
         return <div className='product'>
+            <div>
+                <a href='#' onClick={this.toggleExpanded}>
+                { this.state.expanded ? '-' : '+' }
+                </a>
+            </div>
             <div className='details'>
                 <div className='name'>{this.props.product.name}</div>
-                <div className='desc'>{this.props.product.description}</div>
+                 { this.state.expanded && <div className='desc'>{this.props.product.description}</div> }
             </div>
             <div className='actions'>
                 <div className='remove' title='fix me' onClick={() => this.props.removeProduct(this.props.product)}>x</div>
